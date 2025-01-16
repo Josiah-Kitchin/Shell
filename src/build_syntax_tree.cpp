2d0f35c8f65fbce8715a217636361825b0658151
josiah <josiahd.kitchin@gmail.com>
2025-01-16 00:40:01 -0800

    built a syntax tree

---
+++ src/build_syntax_tree.cpp
@@ -0,0 +1,55 @@
+
+
+#include <sstream> 
+#include <cassert> 
+#include "syntax_tree.hpp"
+
+
+std::vector<std::string> tokenize(const std::string &line) { 
+    /* Transform the string into a vector of tokens */ 
+    std::vector<std::string> words; 
+    std::stringstream stream(line);
+    std::string word; 
+
+    while (stream >> word) { 
+        words.push_back(word);
+    }
+
+    return words; 
+}
+
+TokenNode::TokenNode(Token type) : type(type) {}
+Command TokenNode::get_command() { 
+    assert(type == Token::COMMAND);
+    
+    if (command_line.empty()) { 
+        std::string program = "";
+        return Command(program, command_line);
+    }
+    std::string program = command_line.at(0);
+    return Command(program, command_line);
+}
+
+std::shared_ptr<TokenNode> build_syntax_tree(const std::vector<std::string>& tokens) { 
+    /* Build a syntax tree, with operators being the parents and commands being the leafs. */ 
+
+    auto command_node = std::make_shared<TokenNode>(Token::COMMAND); 
+    for (size_t i = 0; i < tokens.size(); ++i) { 
+        if (tokens[i] == op::PIPE) { 
+            auto pipe_node = std::make_shared<TokenNode>(Token::PIPE);
+            pipe_node->left = command_node; 
+            pipe_node->right = build_syntax_tree(std::vector<std::string>(tokens.begin() + i + 1, tokens.end()));
+            return pipe_node; 
+        } else { 
+            command_node->command_line.push_back(tokens[i]);
+        }
+    }
+    return command_node; 
+}
+
+SyntaxTree::SyntaxTree(const std::string& command_line) { 
+    std::vector<std::string> tokens = tokenize(command_line);
+    m_root = build_syntax_tree(tokens);
+}
+
+
--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -1,6 +1,5 @@
 
 
 #include <gtest/gtest.h> 
-#include "parser.hpp"
 
 
